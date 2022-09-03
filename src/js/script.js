b90736d6698256940aea3e065e9692647b3ef650
Eugene Gladkov <eugvok85@gmail.com>
2022-09-03 17:32:30 +0300

    fix events block

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+(function() {
+   'use strict';
+
+   function trackScroll() {
+      var scrolled = window.pageYOffset;
+      var coords = document.documentElement.clientHeight;
+
+      if (scrolled > coords) {
+         goTopBtn.classList.add('back-to-top-show');
+      }
+      if (scrolled < coords) {
+         goTopBtn.classList.remove('back-to-top-show');
+      }
+   }
+
+   function backToTop() {
+      'use strict';
+      if (window.pageYOffset > 0) {
+         window.scrollBy(0, -80);
+         setTimeout(backToTop, 0);
+      }
+   }
+
+   var goTopBtn = document.querySelector('.back-to-top');
+
+   window.addEventListener('scroll', trackScroll);
+   goTopBtn.addEventListener('click', backToTop);
+})();
